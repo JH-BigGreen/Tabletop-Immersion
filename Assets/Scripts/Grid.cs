af447cb5fd9cb9933cfcb1920d1b6e90da089287
Joel Delight <thefencingflutist@gmail.com>
2017-01-03 21:10:53 -0800

    work on a procedural grid texture.

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Grid : MonoBehaviour {
+
+    public int xSize, zSize;
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    // Use this for initialization
+    void Awake () {
+        Generate();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+
+    private void Generate()
+    {
+
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+               
+            }
+        }
+        mesh.vertices = vertices;
+        int[] triangles = new int[xSize * zSize * 6];
+
+        for (int ti = 0, vi = 0, z = 0; z < zSize; z++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti + 0] = vi;
+                triangles[ti + 2] = triangles[ti + 3] = vi + 1;
+                triangles[ti + 1] = triangles[ti + 4] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
